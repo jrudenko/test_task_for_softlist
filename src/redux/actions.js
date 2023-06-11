b937c50c663df5128786c7101f8804a9a56cca60
Julia Rudenko <yrudenko@i.ua>
2023-06-11 18:43:35 +0300

    deploy

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -6,7 +6,7 @@ export const fetchData = () => {
     dispatch({ type: "FETCH_DATA_REQUEST" });
 
     axios
-      .get("/api/2.5/plans")
+      .get("https://public.api.openprocurement.org/api/2.5/plans")
       .then((response) => {
         dispatch({
           type: "FETCH_DATA_SUCCESS",
